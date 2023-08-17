1cfef64376e1a7afc6903bc62d376731063450cb
Denver Coneybeare <dconeybe@google.com>
2023-08-17 00:55:03 -0400

    checks.yml: add python code format check (#2)

---
+++ scripts/format.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function print_help {
+cat << EOF
+Syntax: $0 [options]
+
+Options:
+  -h Print this help message.
+  -c Only check the files for formatting errors, but do not modify them;
+       returns an exit code of 0 if no changes would be made, non-zero otherwise.
+EOF
+}
+
+function parse_args {
+  local arg_check_only=0
+
+  while getopts ":ch" arg; do
+    case $arg in
+      h)
+        print_help
+        exit 0
+        ;;
+      c)
+        arg_check_only=1
+        ;;
+      *)
+        echo "ERROR: invalid flag: $OPTARG" >&2
+        echo "Run with -h for help" >&2
+        exit 2
+        ;;
+    esac
+  done
+
+  readonly ARG_CHECK_ONLY=$arg_check_only
+
+  if [[ $OPTIND -le $# ]] ; then
+    echo "ERROR: unexpected argument: $1" >&2
+    echo "Run with -h for help" >&2
+    exit 2
+  fi
+}
+
+function run_pyink {
+  local pyink_args=(
+    pyink
+    --line-length 100
+    --target-version py311
+    --pyink
+    --pyink-indentation 2
+    cmake/*.py
+  )
+
+  if [[ $ARG_CHECK_ONLY == '1' ]] ; then
+    pyink_args+=(
+      --check
+      --diff
+    )
+  fi
+
+  echo "${pyink_args[*]}"
+  exec "${pyink_args[@]}"
+}
+
+parse_args "$@"
+run_pyink
