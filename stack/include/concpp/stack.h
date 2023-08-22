a37f891e55e8a0d94f28ee5860036802da3cddab
Denver Coneybeare <dconeybe@google.com>
2023-08-22 16:59:53 -0400

    start stack (#12)

--- stack/include/concpp/stack.h
+++ stack/include/concpp/stack.h
@@ -1,7 +1,44 @@
 #ifndef CONCPP_STACK_H_
 #define CONCPP_STACK_H_
 
+#include <atomic>
+#include <optional>
+#include <utility>
+
+namespace concpp {
+
+template<typename T>
 class LockFreeStack final {
+public:
+  void push(T value) {
+    auto *new_node = new Node;
+    new(new_node->value) T(std::move(value));
+    new_node->next = head_.load();
+    while (!head_.compare_exchange_weak(new_node->next, new_node));
+  }
+
+  std::optional<T> pop() {
+    Node *old_head = head_.load();
+    while (true) {
+      if (old_head->next == nullptr) {
+        return std::nullopt;
+      }
+      if (head_.compare_exchange_weak(old_head, old_head->next)) {
+        break;
+      }
+    }
+    return T(std::move(*reinterpret_cast<T *>(old_head->value)));
+  }
+
+private:
+  struct Node {
+    alignas(T) char value[sizeof(T)];
+    Node *next = nullptr;
+  };
+
+  std::atomic<Node *> head_ = new Node;
 };
+
+} // namespace concpp
 
 #endif  // CONCPP_STACK_H_
--- stack/test/stack_test.cc
+++ stack/test/stack_test.cc
@@ -2,5 +2,19 @@
 
 #include "gtest/gtest.h"
 
-TEST(Stack, DummyTest) {
+using concpp::LockFreeStack;
+
+namespace {
+
+TEST(Stack, Push3Pop3Int) {
+  LockFreeStack<int> stack;
+  for (int i=0; i<5; i++) {
+    stack.push(i);
+  }
+  for (int i=4; i>=0; i--) {
+    EXPECT_EQ(stack.pop().value(), i);
+  }
 }
+
+}  // namespace
+
