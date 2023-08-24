6cd68b5c44da641aa7b807d71a26f798206cff63
Denver Coneybeare <dconeybe@google.com>
2023-08-24 12:32:01 -0400

    scripts/test.sh: refactor to run all tests even in the presence of failures

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,30 @@
 
 set -euo pipefail
 
-for f in cmake/*.py ; do
+pass_count=0
+fail_count=0
+
+for f in cmake/*.test.py ; do
   echo "Running test: $f"
-  python "$f"
+  if python "$f" ; then
+    pass_count=$((pass_count + 1))
+  else
+    fail_count=$((fail_count + 1))
+  fi
 done
+
+echo
+echo '------------'
+echo 'TEST SUMMARY'
+echo '------------'
+echo "Tests Run:    $((pass_count+fail_count))"
+echo "Tests Passed: ${pass_count}"
+echo "Tests Failed: ${fail_count}"
+
+if [[ fail_count -gt 0 ]] ; then
+  echo
+  echo '***************************************************************'
+  echo '** TEST FAILURES OCCURRED'
+  echo '***************************************************************'
+  exit 1
+fi
