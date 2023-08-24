a9cce874f3cf6e392164227cb1d86c344e627f0e
Denver Coneybeare <dconeybe@google.com>
2023-08-23 22:53:11 -0400

    move some logic from the python script into the template

--- workflows/checks/generate.py
+++ workflows/checks/generate.py
@@ -21,81 +21,41 @@ def main(argv: list[str]) -> None:
 
   template = env.get_template("index.txt")
 
-  rendered_template = template.render(cpp_tests=cpp_tests())
+  rendered_template = template.render(
+      BuildConfig=BuildConfig,
+      Compiler=Compiler,
+      CppTestParameters=CppTestParameters,
+      OperatingSystem=OperatingSystem,
+  )
 
   print(rendered_template)
 
 
+@enum.unique
+class OperatingSystem(enum.Enum):
+  LINUX = enum.auto()
+  MACOS = enum.auto()
+  WINDOWS = enum.auto()
+
+
+@enum.unique
+class BuildConfig(enum.Enum):
+  RELEASE = enum.auto()
+  DEBUG = enum.auto()
+
+
+@enum.unique
+class Compiler(enum.Enum):
+  GCC = enum.auto()
+  CLANG = enum.auto()
+
+
 @dataclasses.dataclass(frozen=True)
 class CppTestParameters:
-  os_name: str
-  github_runs_on: str
-  build_config: str
-  compiler: str | None = None
+  operating_system: OperatingSystem
+  build_config: BuildConfig
+  compiler: Compiler | None = None
   target_architecture: str | None = None
-
-
-def cpp_tests() -> list[CppTestParameters]:
-  return [
-    CppTestParameters(
-      os_name="linux",
-      github_runs_on="ubuntu-latest",
-      build_config="Debug",
-      compiler="gcc",
-    ),
-    CppTestParameters(
-      os_name="linux",
-      github_runs_on="ubuntu-latest",
-      build_config="Release",
-      compiler="gcc",
-    ),
-    CppTestParameters(
-      os_name="linux",
-      github_runs_on="ubuntu-latest",
-      build_config="Debug",
-      compiler="clang",
-    ),
-    CppTestParameters(
-      os_name="linux",
-      github_runs_on="ubuntu-latest",
-      build_config="Release",
-      compiler="clang",
-    ),
-    CppTestParameters(
-      os_name="macos",
-      github_runs_on="macos-latest",
-      build_config="Debug",
-    ),
-    CppTestParameters(
-      os_name="macos",
-      github_runs_on="macos-latest",
-      build_config="Release",
-    ),
-    CppTestParameters(
-      os_name="windows",
-      github_runs_on="windows-latest",
-      build_config="Debug",
-      target_architecture="Win32",
-    ),
-    CppTestParameters(
-      os_name="windows",
-      github_runs_on="windows-latest",
-      build_config="Release",
-      target_architecture="Win32",
-    ),
-    CppTestParameters(
-      os_name="windows",
-      github_runs_on="windows-latest",
-      build_config="Debug",
-      target_architecture="x64",
-    ),
-    CppTestParameters(
-      os_name="windows",
-      github_runs_on="windows-latest",
-      build_config="Release",
-      target_architecture="x64",
-    ),
-  ]
 
 
 if __name__ == "__main__":
