2e951ff3f81c8f54ac9c3d45259b6cb7eed4129a
Denver Coneybeare <dconeybe@google.com>
2023-09-17 22:30:25 -0400

    stack pare down in preparation for rewrite

--- src/include/concpp/stack.h
+++ src/include/concpp/stack.h
@@ -2,82 +2,61 @@
 #define CONCPP_STACK_H_
 
 #include <atomic>
-#include <optional>
+#include <memory>
+#include <type_traits>
 #include <utility>
 
 namespace concpp {
 
-/**
- * A stack data structure that is lock-free.
- *
- * The `head_` pointer always points to a non-null object. Initially, it points to a "sentinel"
- * object. The "sentinel" can be identified because its `next` pointer is `nullptr`.
- */
 template<typename T>
-class LockFreeStack final {
-public:
-  ~LockFreeStack() {
-    Node* head = head_.load();
-    while (head) {
-      Node* next = head->next;
+class stack final {
+ public:
+  stack() : head_(new node) {
+  }
+
+  ~stack() {
+    node* head = head_.load();
+    while (true) {
+      if (!head) {
+        break;
+      }
+      node* next = head->next;
       delete head;
       head = next;
     }
   }
 
   void push(T value) {
-    auto *new_node = new Node(std::move(value), head_.load());
-    while (!head_.compare_exchange_weak(new_node->next, new_node));
+    return push(std::make_unique<T>(std::move(value)));
   }
 
-  std::optional<T> pop() {
-    Node *old_head = head_.load();
-    while (true) {
-      if (!old_head->valid) {
-        return std::nullopt;
-      }
-      if (head_.compare_exchange_weak(old_head, old_head->next)) {
-        break;
-      }
-    }
-    return std::move(*old_head).value();
+  void push(std::unique_ptr<T>) {
+
   }
 
-private:
-  struct Node {
-    Node() = default;
-    Node(const Node&) = delete;
-    Node& operator=(const Node&) = delete;
+  std::unique_ptr<T> pop() {
+    return {};
+  }
 
-    Node(T value, Node* next_) : valid(true), next(next_) {
-      new(value_) T(std::move(value));
-    }
+  // Move and delete copy constructors and assignment operators are not supported.
+  stack(const stack&) = delete;
+  stack(stack&&) = delete;
+  stack& operator=(const stack&) = delete;
+  stack& operator=(stack&&) = delete;
 
-    ~Node() {
-      if (valid) {
-        value().~T();
-      }
-    }
+ private:
+  struct node {
+    std::unique_ptr<T> data;
+    node* next = nullptr;
+  }; // class node
 
-    const T& value() const& {
-      return *reinterpret_cast<const T*>(value_);
-    }
+  [[maybe_unused]]
+  void type_traits_static_asserts() {
+    static_assert(decltype(head_)::is_always_lock_free);
+  }
 
-    T& value() & {
-      return *reinterpret_cast<T*>(value_);
-    }
-
-    T&& value() && {
-      return std::move(*reinterpret_cast<T*>(value_));
-    }
-
-    bool valid = false;
-    Node *next = nullptr;
-    alignas(T) char value_[sizeof(T)]{};
-  };
-
-  std::atomic<Node *> head_ = new Node;
-};
+  std::atomic<node*> head_;
+}; // class stack
 
 } // namespace concpp
 
