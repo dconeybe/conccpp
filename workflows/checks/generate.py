9afe0a9a6501689d3ec21d8432db7f5ad64e7ceb
Denver Coneybeare <dconeybe@google.com>
2023-09-27 15:17:07 -0400

    refactor generate.py to not use custom types

--- workflows/checks/generate.py
+++ workflows/checks/generate.py
@@ -1,9 +1,10 @@
-import dataclasses
-import enum
 import pathlib
+import re
 
 from absl import app
 import jinja2
+import jinja2.ext
+import jinja2.exceptions
 
 
 def main(argv: list[str]) -> None:
@@ -19,45 +20,49 @@ def main(argv: list[str]) -> None:
       lstrip_blocks=True,
       trim_blocks=True,
       loader=jinja2.FileSystemLoader(template_dir),
+      undefined=jinja2.StrictUndefined,
+      extensions=[RaiseExtension],
   )
+  env.filters["replace_whitespace"] = replace_whitespace
 
   template = env.get_template("index.txt")
 
-  rendered_template = template.render(
-      BuildConfig=BuildConfig,
-      Compiler=Compiler,
-      CppTestParameters=CppTestParameters,
-      OperatingSystem=OperatingSystem,
-  )
+  rendered_template = template.render()
 
   print(rendered_template)
 
 
-@enum.unique
-class OperatingSystem(enum.Enum):
-  LINUX = enum.auto()
-  MACOS = enum.auto()
-  WINDOWS = enum.auto()
+def replace_whitespace(value: str, replacement: str) -> str:
+  return re.sub(r"\s+", replacement, value, flags=re.DOTALL)
 
 
-@enum.unique
-class BuildConfig(enum.Enum):
-  RELEASE = enum.auto()
-  DEBUG = enum.auto()
+# Adapted from https://github.com/duelafn/python-jinja2-apci/blob/master/jinja2_apci/error.py
+class RaiseExtension(jinja2.ext.Extension):
+  """
+  A Jinja2 extension that adds a "raise" tag to the language, which takes a single string argument
+  and raises an exception.
 
+  e.g. {% raise "Should never get here" %}
+  """
 
-@enum.unique
-class Compiler(enum.Enum):
-  GCC = enum.auto()
-  CLANG = enum.auto()
+  tags = set(["raise"])
 
+  def parse(self, parser):
+    # the first token is the token that started the tag. In our case we
+    # only listen to "raise" so this will be a name token with
+    # "raise" as value. We get the line number so that we can give
+    # that line number to the nodes we insert.
+    lineno = next(parser.stream).lineno
 
-@dataclasses.dataclass(frozen=True)
-class CppTestParameters:
-  operating_system: OperatingSystem
-  build_config: BuildConfig
-  compiler: Compiler | None = None
-  target_architecture: str | None = None
+    # Extract the message from the template
+    message_node = parser.parse_expression()
+
+    return jinja2.nodes.CallBlock(
+        self.call_method("_raise", [message_node], lineno=lineno), [], [], [], lineno=lineno
+    )
+
+  def _raise(self, msg, caller):
+    raise jinja2.exceptions.TemplateRuntimeError(msg)
 
 
 if __name__ == "__main__":
