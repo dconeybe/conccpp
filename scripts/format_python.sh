ff14b7c28fa92139cae264e0796a4f03b87d916a
Denver Coneybeare <dconeybe@google.com>
2023-10-05 01:27:25 -0400

    rename pyformat to format_python

--- scripts/format_python.sh
+++ scripts/format_python.sh
@@ -9,5 +9,5 @@ exec \
   --mount "type=bind,src=$PWD,dst=$PWD" \
   --workdir "$PWD" \
   docker.io/lakeoak/conccpp_python:2 \
-  python -m scripts.pyformat \
+  python -m scripts.format_python \
   "$@"
